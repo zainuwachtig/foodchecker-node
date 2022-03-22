66d16ab387f6c193cad459aa92d230b618c6cdc9
zainuwachtig <zain.butt@hva.nl>
2022-03-22 14:42:47 +0100

    snelle camera fix

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 const https = require('https');
 const express = require('express');
 const app = express();
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 const hostname = '127.0.0.1';
 const port = 5500;
 
-// Get data from api
-https.get('https://world.openfoodfacts.org/api/v0/product/8713500010456', (resp) => {
-  let data = '';
-
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
 // Template engine
 app.set('view engine', 'ejs');
 let ejs = require('ejs');
+const { render } = require('express/lib/response');
+
+app.use(express.static('static'))
 
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.get('/product/:barcode', async function(req, res) {
+  const baseUrl = 'https://world.openfoodfacts.org/api/v0/product/';
+  const barcode = req.params.barcode;
+ 
+  return await fetch(baseUrl + barcode + '.json')
+    .then((response) => response.json())
+    .then((data) => res.render('product', { data }))
+    .catch((error) => error) 
+})
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ static/js/script.js
@@ -0,0 +1,39 @@
+async function detectCamera() {
+    const camera = document.querySelector('video');
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: {
+            ideal: "enviroment"
+            }
+        },
+    });
+    camera.srcObject = stream;
+  
+    // Als de camera is toegestaan wordt de melding weggehaald
+    if (stream.getVideoTracks().length > 0) {
+      const melding = document.querySelector('.melding');
+      melding.classList.add('onzichtbaar')
+    }
+    
+    await camera.play();
+    detectCamera(camera)
+};
+
+// https://dev.to/ycmjason/detecting-barcode-from-the-browser-d7n
+async function detectBarcode(camera) {
+    window.setInterval(async () => {
+      const barcodeDetector = new BarcodeDetector();
+      const barcodes = await barcodeDetector.detect(camera);
+      // Als de length dus 0 of kleiner is, is er geen product gevonden en zoekt die verder.
+      if (barcodes.length <= 0) {
+        return;
+      // Wanneer een product gevonden is, wordt dat in een array gezet en daar wordt de eerste dan uitgehaald, wat de gescande barcode is.
+      } else {
+        const barcode = barcodes[0].rawValue
+        // Hiermee voert die getData(barcode) uit
+        window.location.pathname = 'product/' + barcode;
+      }
+    }, 1000)
+};
+
+detectCamera();
